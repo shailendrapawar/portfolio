a0d02cabf0f467320785b83cac57cf1e7e3698ee
shailendr pawar <shailendrapawar980@gmail.com>
2025-06-01 00:14:08 +0530

    added: navbar with functionality of theme handling

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router"
 import Navbar from "./components/navbar/Navbar"
+import { useSelector } from "react-redux"
 
 function RootLayout() {
+  const {currentTheme}=useSelector(s=>s.theme)
   return (
-    <div className="bg- min-h-screen w-screen relative flex flex-col items-center">
+    <div className="bg- min-h-screen w-screen relative flex flex-col items-center"
+    style={{color:currentTheme.textPrimary}}
+    >
 
       <Navbar/>
       <div className=" w-[100%]">
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,71 @@
-import { useSelector } from "react-redux"
-import { NavLink } from "react-router"
+import "./navbar.css"
+import { useDispatch, useSelector } from "react-redux"
+import { NavLink, useNavigate } from "react-router"
+
+
+import { toggleTheme } from "../../store/slices/themeSlice.js"
+import { useEffect, useState } from "react";
+
+import { FaDollarSign } from "react-icons/fa6";
+import { MdSunny } from "react-icons/md";
+import { HiMiniMoon } from "react-icons/hi2";
+import { CgMenuRight } from "react-icons/cg";
+import { RxCross2 } from "react-icons/rx";
+
 
 function Navbar() {
-  const {currentTheme}=useSelector(s=>s.theme)
+
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch();
+
+  const [toggle, setToggle] = useState(false);
+
+  const { currentTheme } = useSelector(s => s.theme)
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+    const isDay = hour >= 6 && hour < 18; // Daytime between 6 AM and 6 PM
+
+    console.log(isDay);
+
+    // dispatch(setTheme(isDay ? "light" : "dark"));
+  }, []);
+
+
   return (
-    <nav className="h-13 w-[90%] max-w-200 bg-white mt-5 rounded-4xl flex justify-center items-center"
-    style={{backgroundColor:currentTheme.cardBackground,border:`1px solid ${currentTheme.border}`,boxShadow:` 2px 2px 10px ${currentTheme.shadow}`}}
+    <nav className=" cursor-pointer h-13 w-[90%] max-w-200 bg-white mt-5 rounded-4xl flex justify-center items-center relative"
+      style={{ backgroundColor: currentTheme.cardBackground, border: `1px solid ${currentTheme.border}`, boxShadow: ` 2px 2px 10px ${currentTheme.shadow}` }}
+      onClick={()=>setToggle(false)}
     >
-      
-      <nav className="web-nav flex justify-center items-center gap-5">
-        <NavLink to={"/"} className={({isActive})=>`w-20 h-8 flex justify-center items-center`} style={({isActive})=>isActive?{borderBottom:`3px solid ${currentTheme.accent}`}:{borderBottom:`3px solid transparent`}}>Home</NavLink>
-        <NavLink to={"/projects"} className={({isActive})=>`w-20 h-8 flex justify-center items-center`} style={({isActive})=>isActive?{borderBottom:`3px solid ${currentTheme.accent}`}:{borderBottom:`3px solid transparent`}}>Projects</NavLink>
-        <NavLink to={"/contact"} className={({isActive})=>`w-20 h-8 flex justify-center items-center`} style={({isActive})=>isActive?{borderBottom:`3px solid ${currentTheme.accent}`}:{borderBottom:`3px solid transparent`}}>Contact</NavLink>
+
+      <FaDollarSign className="h-7 w-7 absolute left-5" onClick={(e) =>{e.stopPropagation();  navigate("/")}} />
+
+      <nav className="web-nav flex justify-center items-center gap-5 ">
+        <NavLink to={"/"} className={({ isActive }) => `w-20 h-8 flex justify-center items-center`} style={({ isActive }) => isActive ? { borderBottom: `3px solid ${currentTheme.accent}` } : { borderBottom: `3px solid transparent` }}>Home</NavLink>
+        <NavLink to={"/projects"} className={({ isActive }) => `w-20 h-8 flex justify-center items-center`} style={({ isActive }) => isActive ? { borderBottom: `3px solid ${currentTheme.accent}` } : { borderBottom: `3px solid transparent` }}>Projects</NavLink>
+        <NavLink to={"/contact"} className={({ isActive }) => `w-20 h-8 flex justify-center items-center`} style={({ isActive }) => isActive ? { borderBottom: `3px solid ${currentTheme.accent}` } : { borderBottom: `3px solid transparent` }}>Contact</NavLink>
       </nav>
 
 
-      
+      <nav className={`mobile-nav h-25 w-[80%] absolute -bottom-25 rounded-b-2xl hidden`} style={toggle ? { backgroundColor: currentTheme.cardBackground, } : { display: "none" }}>
+
+      </nav>
+
+
+
+      <span className={` theme-toggle h-5 w-10 rounded-xl flex items-center absolute right-10 ${currentTheme.name == "light" ? "justify-start" : "justify-end"} pl-1 pr-1`}
+        onClick={(e) =>{e.stopPropagation(); dispatch(toggleTheme())}}
+        style={{ backgroundColor: currentTheme.secondaryAccent }}
+      >
+        {currentTheme.name == "light" && (<MdSunny className={`text-white rotateRight `} />)}
+        {currentTheme.name == "dark" && (<HiMiniMoon className={`text-white rotateLeft `} />)}
+      </span>
+
+      {!toggle && (<CgMenuRight className=" rotateRight mobileNav-toggler absolute right-5 h-7 w-7 " onClick={(e) =>{e.stopPropagation();  setToggle(!toggle)}} />)}
+      {toggle && (<RxCross2 className=" rotateLeft mobileNav-toggler absolute right-5 h-7 w-7" onClick={(e) =>{e.stopPropagation();  setToggle(!toggle)}} />)}
+
+
     </nav>
   )
 }
