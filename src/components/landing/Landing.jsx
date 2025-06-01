b7d625e774f906aee6553630ffd3472d6e5f9164
shailendr pawar <shailendrapawar980@gmail.com>
2025-06-01 15:18:11 +0530

    improved: ui for andd responsiveness for landing page

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -6,41 +6,59 @@ import lightWizzard from "../../assets/blue-wizard.png"
 import darkWizzard from "../../assets/white-wizard.png"
 import MagicBall from "../magicBall/MagicBall";
 
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
+const urls={
+  linkedin:"https://www.linkedin.com/in/shailendra-pawar792/",
+  github:"https://github.com/shailendrapawar/",
+  facebook:"https://www.facebook.com/shailendra.pawar.50159",
+  leetcode:"https://leetcode.com/u/shailendrapawar/",
+
+  resume:"https://drive.google.com/drive/folders/1-OdardWOtvSyZfOf8WlEV3P45mQkgqAH?usp=drive_link",
+
+
+
+}
+
 function Landing() {
   const { currentTheme } = useSelector(s => s.theme);
 
   return (
     <div className="relative min-h-[calc(100vh-80px)] flex justify-center ">
-      <section className=" min-h-[calc(100vh-80px)] w-full max-w-250 grid-background ">
+      <section className=" min-h-[calc(100vh-80px)] w-full max-w-250 grid-background flex flex-col sm:flex-row">
 
 
 
-        <main className="h-[45%] w-full p-3 flex flex-col justify-center items-start gap-5">
-          <h3 className="text-xl" style={{ color: currentTheme.textPrimary }}>Hi, I'm <span style={{ color: currentTheme.accent }}>Shailendra Pawar</span></h3>
+        <main className="h-[45%] w-full p-3 flex flex-col justify-center items-start gap-8 sm:h-full sm:w-[50%] sm:gap-15 sm:pl-10 md:pl-5">
+          <h3 className="text-xl sm:text-2xl md:text-4xl" style={{ color: currentTheme.textPrimary }}>Hi, I'm <span style={{ color: currentTheme.accent }}>Shailendra Pawar</span></h3>
 
-          <h1 className="text-3xl" style={{ color: currentTheme.secondaryAccent }}>Full Stack Developer</h1>
+          <h1 className="text-3xl sm:text-5xl md:text-6xl " style={{ color: currentTheme.secondaryAccent }}>Full Stack Developer</h1>
 
-          <p className="w-[80%] text-sm" style={{ color: currentTheme.textSecondary }}>MERN stack wizard ,
+          <p className="w-[80%] text-sm sm:text-lg md:text-xl" style={{ color: currentTheme.textSecondary }}>MERN stack wizard ,
             with a knack for real-time features,
             and seemless user experience</p>
 
-          <div className="h-9  w-auto flex gap-2 " style={{ color: currentTheme.textPrimary }}>
-            <button className="w-25 h-full cursor-pointer rounded-tl-3xl text-white" style={{ backgroundColor: currentTheme.primary }}>Resume</button>
-            <button className="w-25 h-full cursor-pointer rounded-br-3xl" style={{ border: ` 2px solid ${currentTheme.primary}` }}>Let's talk?</button>
+          <div className="h-10 w-55 sm:w-65 sm:h-12 md:w-80 md:h-14 flex gap-2 " style={{ color: currentTheme.textPrimary }}>
+            <button className="w-[50%] h-full cursor-pointer rounded-tl-3xl text-white transition-all hover:w-[70%] sm:text-lg md:text-2xl " style={{ backgroundColor: currentTheme.primary }}><a href={urls.resume} target="_blank">Resume</a></button>
+            <button className="w-[50%] h-full cursor-pointer rounded-br-3xl sm:text-lg  md:text-2xl transition-all hover:w-[70%]" style={{ border: ` 2px solid ${currentTheme.primary}` }}>Let's talk?</button>
           </div>
         </main>
 
 
-        <aside className="h-[55%] relative flex justify-center items-center">
-          <div className="h-60 w-50 relative flex items-center justify-center">
-            <MagicBall extraClasses={"-top-8 left-2 magicBall-anime "}  delay={0.7}/>
-            <MagicBall extraClasses={"-top-2 -right-5 magicBall-anime  "} delay={0.1}  />
+        <aside className="h-[55%] relative flex justify-center items-center   sm:h-full sm:w-[50%] sm:pr-10">
+          <div className="h-60 w-50 relative flex items-center justify-center sm:h-80 sm:w-65 md:h-95 md:w-80">
+            <MagicBall extraClasses={"-top-8 left-2 magicBall-anime "}  delay={0.7} icon={<a href={urls.github} target="_blank" className="h-full w-full"><FaGithub className="h-full w-full text-black"/></a>}/>
+            <MagicBall extraClasses={"-top-2 -right-5 magicBall-anime  "} delay={0.1} icon={<a href={urls.linkedin} target="_blank" className="h-full w-full"><FaLinkedin className="h-full w-full text-black"/></a>}  />
+
             <img src={currentTheme.name == "light" ? `${lightWizzard}` : `${darkWizzard}`}
-              className=" h-60 w-50  "
+              className=" h-full w-full  "
             ></img>
 
-            <MagicBall extraClasses={"-left-8 bottom-10 magicBall-anime "}  delay={0.5} />
-            <MagicBall extraClasses={"-right-4 bottom-2 magicBall-anime"}  delay={0.2}/>
+            <MagicBall extraClasses={"-right-4 bottom-2 magicBall-anime"}  delay={0.2} icon={<a href={urls.leetcode} target="_blank" className="h-full w-full"><SiLeetcode className="h-full w-full text-black"/></a>}/>
+            <MagicBall extraClasses={"-left-8 bottom-10 magicBall-anime "}  delay={0.5} icon={<a href={urls.facebook} target="_blank" className="h-full w-full"><FaFacebook className="h-full w-full text-black"/></a>} />
           </div>
         </aside>
 
--- src/components/magicBall/MagicBall.jsx
+++ src/components/magicBall/MagicBall.jsx
@@ -1,14 +1,14 @@
 import { useSelector } from "react-redux"
 
-function MagicBall({extraClasses ,delay}) {
+function MagicBall({extraClasses ,delay,icon}) {
     const {currentTheme}=useSelector(s=>s.theme);
     const glowClass="shadow-[0_0_20px_5px_rgba(37,99,235,0.8)] animate-pulse rounded-full bg-[#2563EB]"
 
   return (
-    <div className={`absolute ${extraClasses} h-15 w-15 rounded-full transition-all ease-in-out ${glowClass} `} style={{backgroundColor:currentTheme.accent,animationDelay:`${delay}s`}}
+    <div className={`absolute ${extraClasses} h-15 md:h-20  hover:scale-110  w-15 md:w-20 flex justify-center items-center p-3 md:p-5 rounded-full transition-all ease-in-out ${glowClass} `} style={{backgroundColor:currentTheme.accent,animationDelay:`${delay}s`}}
     s
     >
-
+        {icon?icon:<></>}
     </div>
 
 
